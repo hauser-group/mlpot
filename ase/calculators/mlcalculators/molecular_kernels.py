fd6539be6c73e1d80a6a151ee091ab24ebdb0695
Ralf Meyer <ralf.meyer@tugraz.at>
2019-04-08 17:33:36 +0200

    CHG: Introduce RBF kernel without factor as hyperparameter

--- ase/calculators/mlcalculators/molecular_kernels.py
+++ ase/calculators/mlcalculators/molecular_kernels.py
@@ -144,6 +144,137 @@ class KlemensInterface(object):
 class RBFKernel():
 
     def __init__(self, constant=1.0, factor=1.0, length_scale=1.0,
+            length_scale_bounds=(1e-5, 1e5)):
+        self.factor = factor
+        self.constant = constant
+        self.length_scale = length_scale
+        self.length_scale_bounds = length_scale_bounds
+
+    @property
+    def anisotropic(self):
+        return np.iterable(self.length_scale) and len(self.length_scale) > 1
+
+    @property
+    def theta(self):
+        return np.log(self.length_scale)
+
+    @theta.setter
+    def theta(self, theta):
+        self.length_scale = np.exp(theta)
+
+    @property
+    def bounds(self):
+        if np.ndim(self.length_scale_bounds) == 1:
+            return np.log(np.asarray[self.length_scale_bounds])
+        elif np.ndim(self.length_scale_bounds) == 2:
+            return np.log(self.length_scale_bounds)
+        else:
+            raise ValueError('Unexpected dimension of length_scale_bounds')
+
+    def __call__(self, atoms_X, atoms_Y, dx=False, dy=False, eval_gradient=False):
+        n = len(atoms_X)
+        m = len(atoms_Y)
+        n_dim = 3*len(atoms_X[0])
+
+        X = np.zeros((len(atoms_X), n_dim))
+        Y = np.zeros((len(atoms_Y), n_dim))
+        for i, atoms in enumerate(atoms_X):
+            X[i,:] = atoms.get_positions().flatten()
+        for i, atoms in enumerate(atoms_Y):
+            Y[i,:] = atoms.get_positions().flatten()
+
+        # The arguments dx and dy are deprecated and will be removed soon
+        if not (dx and dy):
+            raise NotImplementedError
+        # Initialize kernel matrix
+        K = np.zeros((n*(1+n_dim), m*(1+n_dim)))
+        if eval_gradient:
+            # Array to hold the derivatives with respect to the length_scale
+            if self.anisotropic:
+                K_gradient = np.zeros((n*(1+n_dim), m*(1+n_dim),
+                    self.length_scale.shape[0]))
+            else: # isotropic
+                K_gradient = np.zeros((n*(1+n_dim), m*(1+n_dim), 1))
+        for a, Xa in enumerate(X):
+            for b, Yb in enumerate(Y):
+                # Index ranges for the derivatives are given by the following
+                # slice objects:
+                da = slice(n+a*n_dim, n+(a+1)*n_dim, 1)
+                db = slice(m+b*n_dim, m+(b+1)*n_dim, 1)
+                # A few helpful quantities:
+                scaled_diff = (Xa-Yb)/self.length_scale
+                inner_prod = scaled_diff.dot(scaled_diff)
+                outer_prod = np.outer(scaled_diff, scaled_diff)
+                outer_prod_over_l = np.outer(scaled_diff/self.length_scale,
+                    scaled_diff/self.length_scale)
+                exp_term = np.exp(-.5*inner_prod)
+                # populate kernel matrix:
+                K[a, b] = exp_term
+                K[da, b] = -exp_term*scaled_diff/self.length_scale
+                K[a, db] = exp_term*scaled_diff/self.length_scale
+                K[da, db] = exp_term*(
+                    np.eye(n_dim)/self.length_scale**2 - outer_prod_over_l)
+
+                # Gradient with respect to the length_scale
+                if eval_gradient:
+                    if self.anisotropic:
+                        # Following the accompaning latex documents the
+                        # three matrix dimensions are refered to as q, p and s.
+                        K_gradient[a, b, :] = exp_term*(
+                            scaled_diff**2/self.length_scale)
+                        K_gradient[da, b, :] = exp_term*(
+                            2*np.diag(scaled_diff/self.length_scale**2) -
+                            np.outer(scaled_diff/self.length_scale,
+                            scaled_diff**2/self.length_scale))
+                        K_gradient[a, db, :] = -exp_term*(
+                            2*np.diag(scaled_diff/self.length_scale**2) -
+                            np.outer(scaled_diff/self.length_scale,
+                            scaled_diff**2/self.length_scale))
+                        delta_qp_over_lq2 = np.repeat((np.eye(n_dim)/
+                            self.length_scale**2)[:, :, np.newaxis],
+                            n_dim, axis=2)
+                        delta_qs = np.repeat(
+                            np.eye(n_dim)[:, np.newaxis, :], n_dim, axis=1)
+                        delta_ps = np.repeat(
+                            np.eye(n_dim)[np.newaxis, :, :], n_dim, axis=0)
+                        scaled_diff_s_squared = np.tile(
+                            scaled_diff**2, (n_dim, n_dim, 1))
+                        K_gradient[da, db, :] = exp_term*(delta_qp_over_lq2*(
+                            scaled_diff_s_squared - 2*delta_qs) +
+                            np.repeat(np.outer(scaled_diff/self.length_scale,
+                            scaled_diff/self.length_scale)[:, :, np.newaxis],
+                            n_dim, axis=2)*
+                            (2*delta_qs + 2*delta_ps - scaled_diff_s_squared)
+                            )/self.length_scale
+                    else: # isotropic
+                        K_gradient[a, b, 0] = exp_term*(
+                            inner_prod/self.length_scale)
+                        K_gradient[da, b, 0] = exp_term*(
+                            scaled_diff*(2 - inner_prod)/self.length_scale**2)
+                        K_gradient[a, db, 0] = -exp_term*(
+                            scaled_diff*(2 - inner_prod)
+                            /self.length_scale**2)
+                        K_gradient[da, db, 0] = exp_term*(
+                            np.eye(n_dim)*(inner_prod - 2) +
+                            outer_prod*(4 - inner_prod))/self.length_scale**3
+
+        if eval_gradient:
+            # Multiply gradient with respect to the length_scale by factor
+            K_gradient *= self.factor
+
+        # Multiply by factor
+        K *= self.factor
+        # Add constant term only on non-derivative block
+        K[:n,:m] += self.constant
+
+        if not eval_gradient:
+            return K
+        else:
+            return K, K_gradient
+
+class RBFKernel_with_factor():
+
+    def __init__(self, constant=1.0, factor=1.0, length_scale=1.0,
             factor_bounds=(1e-5, 1e5), length_scale_bounds=(1e-5, 1e5)):
         self.factor = factor
         self.constant = constant
