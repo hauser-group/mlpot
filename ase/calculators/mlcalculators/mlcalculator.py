bd712367ea4d81e0ee5f96652f8f2d3d30fdb21d
Ralf Meyer <ralf.meyer@tugraz.at>
2019-05-03 15:22:00 +0200

    CHG: Add add_data method to abstract mlcalculator class

--- ase/calculators/mlcalculators/mlcalculator.py
+++ ase/calculators/mlcalculators/mlcalculator.py
@@ -7,20 +7,23 @@ class MLCalculator(Calculator):
     implemented_properties = ['energy', 'forces']
 
     def __init__(self, restart=None, ignore_bad_restart_file=False,
-                 label=None, atoms=None, C1 = 1.0, C2 = 1.0, **kwargs):
+                 label=None, atoms=None, C1=1.0, C2=1.0, **kwargs):
         Calculator.__init__(self, restart, ignore_bad_restart_file, label,
                             atoms, **kwargs)
 
         self.C1 = C1
         self.C2 = C2
-        self.fitted = False
+        self.atoms_train = []
 
     def calculate(self, atoms=None, properties=['energy', 'forces'],
                                 system_changes=all_changes):
         Calculator.calculate(self, atoms, properties, system_changes)
         self.results['energy'], self.results['forces'] = self.predict(atoms)
 
-    def fit(self, atoms_list):
+    def add_data(self, atoms):
+        self.atoms_train.append(atoms)
+
+    def fit(self):
         pass
 
     def predict(self, atoms):
