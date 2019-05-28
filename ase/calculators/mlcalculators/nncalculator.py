a85b479f8ecd68bd46d3af51d8681a708542380a
Ralf Meyer <ralf.meyer@tugraz.at>
2019-05-28 16:41:21 +0200

    CHG: New optimization procedure

--- ase/calculators/mlcalculators/nncalculator.py
+++ ase/calculators/mlcalculators/nncalculator.py
@@ -11,8 +11,9 @@ class NNCalculator(MLCalculator):
                  descriptor_set=None, layers=None, offsets=None,
                  normalize_input=False, model_dir=None, config=None,
                  opt_restarts=1, reset_fit=True, opt_method='L-BFGS-B',
-                 opt_options={'maxiter':1000, 'disp':False, 'gtol':1E-10},
-                 **kwargs):
+                 maxiter=5000, maxcor=200, adam_thresh=100,
+                 adam_learning_rate=5e-3, adam_epsilon=1e-08,
+                 adam_maxiter=10000, **kwargs):
         MLCalculator.__init__(self, restart, ignore_bad_restart_file, label,
                             atoms, C1, C2, **kwargs)
 
@@ -31,7 +32,9 @@ class NNCalculator(MLCalculator):
 
         self.opt_restarts = opt_restarts
         self.reset_fit = reset_fit
-        self.opt_options = opt_options
+        self.maxiter = maxiter
+        self.adam_maxiter = adam_maxiter
+        self.adam_thresh = adam_thresh
 
         self.model_dir = model_dir
         if not normalize_input in ['mean', 'min_max', 'norm', False]:
@@ -70,9 +73,12 @@ class NNCalculator(MLCalculator):
                     .5*self.pot.mse_forces*(self.C2/self.C1)
                     + reg_term/self.C1, name='Loss')
                 self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(
-                    self.loss, method=opt_method, options=opt_options,
+                    self.loss, method=opt_method,
+                    options={'maxiter':maxiter, 'disp':False, 'gtol':1e-10,
+                        'maxcor':maxcor},
                     var_list = self.pot.variables)#[v for v in self.pot.variables if not 'b:' in v.name])
-                self.AdamOpt = tf.train.AdamOptimizer()
+                self.AdamOpt = tf.train.AdamOptimizer(
+                    learning_rate=adam_learning_rate, epsilon=adam_epsilon)
                 self.train = self.AdamOpt.minimize(self.loss)
                 self.init_adam = tf.initializers.variables(self.AdamOpt.variables())
         self.session = tf.Session(config=config, graph=self.graph)
@@ -82,7 +88,8 @@ class NNCalculator(MLCalculator):
     def add_data(self, atoms):
         # If the trainings set is empty: setup the numpy arrays
         if not self.atoms_train:
-            self.n_dim = 3*len(atoms)
+            self.N_atoms = len(atoms)
+            self.n_dim = 3*self.N_atoms
             self.E_train = np.zeros(0)
             self.F_train = []
             self.Gs = []
@@ -101,25 +108,14 @@ class NNCalculator(MLCalculator):
         self.dGs.append(dGi)
 
     def fit(self):
+        global opt_step
         print('Fit called with %d geometries.'%len(self.atoms_train))
         # TODO: This could be streamlined even more by not using the
         # calculate_bp_indices function on the whole set but simply appending
         # to ann_inputs, indices and ann_derivs
+
         ann_inputs, indices, ann_derivs = calculate_bp_indices(
             len(self.atomtypes), self.Gs, self.int_types, dGs=self.dGs)
-
-        if self.normalize_input == 'mean':
-            for i, t in enumerate(self.atomtypes):
-                self.Gs_norm1[t] = np.mean(ann_inputs[i], axis=0)
-                # Small offset for numerical stability
-                self.Gs_norm2[t] = np.std(ann_inputs[i], axis=0) + 1E-6
-        elif self.normalize_input == 'min_max':
-            for i, t in enumerate(self.atomtypes):
-                self.Gs_norm1[t] =  np.min(ann_inputs[i], axis=0)
-                # Small offset for numerical stability
-                self.Gs_norm2[t] = (np.max(ann_inputs[i], axis=0) -
-                    np.min(ann_inputs[i], axis=0) + 1E-6)
-
         self.train_dict = {self.pot.target: self.E_train,
             self.pot.target_forces: self.F_train,
             self.pot.error_weights: np.ones(len(self.atoms_train))}
@@ -139,24 +135,127 @@ class NNCalculator(MLCalculator):
                     self.pot.atomic_contributions[t].derivatives_input
                     ] = np.einsum('ijkl,j->ijkl', ann_derivs[i], 1.0/self.Gs_norm2[t])
 
+        if self.normalize_input == 'mean':
+            for i, t in enumerate(self.atomtypes):
+                self.Gs_norm1[t] = np.mean(ann_inputs[i], axis=0)
+                # Small offset for numerical stability
+                self.Gs_norm2[t] = np.std(ann_inputs[i], axis=0) + 1E-6
+        elif self.normalize_input == 'min_max':
+            for i, t in enumerate(self.atomtypes):
+                self.Gs_norm1[t] =  np.min(ann_inputs[i], axis=0)
+                # Small offset for numerical stability
+                self.Gs_norm2[t] = (np.max(ann_inputs[i], axis=0) -
+                    np.min(ann_inputs[i], axis=0) + 1E-6)
+
+        # batches = []
+        # ind = np.random.permutation(len(self.Gs))
+        # for i in range(0, len(self.Gs), self.batch_size):
+        #     ann_inputs, indices, ann_derivs = calculate_bp_indices(
+        #         len(self.atomtypes),
+        #         [self.Gs[j] for j in ind[i:(i+self.batch_size)]],
+        #         [self.int_types[j] for j in ind[i:(i+self.batch_size)]],
+        #         dGs=[self.dGs[j] for j in ind[i:(i+self.batch_size)]])
+        #     batch_dict = {self.pot.target: self.E_train[ind[i:(i+self.batch_size)]],
+        #         self.pot.target_forces: [self.F_train[i] for i in ind[i:(i+self.batch_size)]],
+        #         self.pot.error_weights: np.ones(self.batch_size)}
+        #     for i, t in enumerate(self.atomtypes):
+        #         batch_dict[self.pot.atom_indices[t]] = indices[i]
+        #         if self.normalize_input == 'norm':
+        #             norm_i = np.linalg.norm(ann_inputs[i], axis=1)
+        #             batch_dict[self.pot.atomic_contributions[t].input] = (
+        #                 ann_inputs[i])/norm_i[:,np.newaxis]
+        #             batch_dict[
+        #                 self.pot.atomic_contributions[t].derivatives_input
+        #                 ] = np.einsum('ijkl,i->ijkl', ann_derivs[i], 1.0/norm_i)
+        #         else:
+        #             batch_dict[self.pot.atomic_contributions[t].input] = (
+        #                 ann_inputs[i]-self.Gs_norm1[t])/self.Gs_norm2[t]
+        #             batch_dict[
+        #                 self.pot.atomic_contributions[t].derivatives_input
+        #                 ] = np.einsum('ijkl,j->ijkl', ann_derivs[i], 1.0/self.Gs_norm2[t])
+        #     batches.append(batch_dict)
+        # def gen():
+        #     ind = np.random.permutation(len(self.Gs))
+        #     for i in range(0, len(self.Gs), self.batch_size):
+        #         ann_inputs, indices, ann_derivs = calculate_bp_indices(
+        #             len(self.atomtypes),
+        #             [self.Gs[j] for j in ind[i:(i+self.batch_size)]],
+        #             [self.int_types[j] for j in ind[i:(i+self.batch_size)]],
+        #             dGs=[self.dGs[j] for j in ind[i:(i+self.batch_size)]]
+        #             )
+        #         out = {'error_weights':np.ones(self.batch_size)}
+        #         for i, t in enumerate(self.atomtypes):
+        #             out['%s_indices'%t] = indices[i]
+        #             if self.normalize_input == 'norm':
+        #                 norm_i = np.linalg.norm(ann_inputs[i], axis=1)
+        #                 out['%s_input'%t] = (
+        #                     ann_inputs[i])/norm_i[:,np.newaxis]
+        #                 out['%s_dervis'%t] = np.einsum(
+        #                     'ijkl,i->ijkl', ann_derivs[i], 1.0/norm_i)
+        #             else:
+        #                 out['%s_input'%t] = (
+        #                     ann_inputs[i]-self.Gs_norm1[t])/self.Gs_norm2[t]
+        #                 out['%s_dervis'%t] = np.einsum(
+        #                     'ijkl,j->ijkl', ann_derivs[i], 1.0/self.Gs_norm2[t])
+        #             out['%s_input'%t] = ann_inputs[i]
+        #             out['%s_dervis'%t] = ann_derivs[i]
+        #
+        #         yield(out, {'energy':self.E_train[ind[i:(i+self.batch_size)]],
+        #             'forces':[self.F_train[i] for i in ind[i:(i+self.batch_size)]]})
+        # input_types = {'error_weights':tf.float64}
+        # for i, t in enumerate(self.atomtypes):
+        #     input_types['%s_indices'%t] = tf.int32
+        #     input_types['%s_input'%t] = tf.float64
+        #     input_types['%s_dervis'%t] = tf.float64
+        # output_types = {'energy':tf.float64,
+        #     'forces':tf.float64}
+        # with self.graph.as_default():
+        #     batched_dataset = tf.data.Dataset.from_generator(gen,
+        #         (input_types, output_types))
+        #     init_train = self.pot.iterator.make_initializer(batched_dataset)
+        #dataset_train = tf.data.Dataset.from_generator(gen,
+        #    ({'Ag_input':tf.float32, 'Ag_indices':tf.int32, 'error_weights':tf.float32},
+        #    {'energy':tf.float32}),
+        #    ({'Ag_input':tf.TensorShape([None, len(Gs_train[0][0])]),
+        #    'Ag_indices':tf.TensorShape([None,1]),
+        #    'error_weights':tf.TensorShape([None])},
+        #    {'energy':tf.TensorShape([None,])}))
+
         # Start with large minimum loss value
         min_loss_value = 1E20
         for i in range(self.opt_restarts):
             # Reset weights to random initialization:
             if (i > 0 or self.reset_fit or
-                  (self.opt_restarts == 1 and self.reset_fit)):
+                    (self.opt_restarts == 1 and self.reset_fit)):
                 self.session.run(tf.initializers.variables(self.pot.variables))
-
                 # Do a few steps of ADAM optimization to start off:
+                print('Starting ADAM optimization for %d epochs'%(
+                    self.adam_maxiter))
                 self.session.run(self.init_adam)
-                for _ in range(self.opt_options['maxiter']):
+                for epoch in range(self.adam_maxiter):
+                    #self.session.run(init_train)
+                    #while True:
+                    #    try:
+                    #        self.session.run(self.train)
+                    #    except tf.errors.OutOfRangeError:
+                    #        break
+                    #for b in sample(batches, len(batches)):
+                    #    self.session.run(self.train, b)
                     self.session.run(self.train, self.train_dict)
-                loss_value, e_rmse, f_rmse = self.session.run(
-                    [self.loss, self.pot.rmse, self.pot.rmse_forces],
-                    self.train_dict)
-                print('Finished Adam optimization. '
-                    'Total loss = %f, RMSE energy = %f, RMSE forces = %f.'%(
-                        loss_value, e_rmse, f_rmse))
+                    if epoch%100 == 0:
+                        loss_value, e_rmse, f_rmse = self.session.run(
+                            [self.loss, self.pot.rmse, self.pot.rmse_forces],
+                            self.train_dict)
+                        # if epoch%1000 == 0:
+                        #     print(epoch, loss_value, e_rmse, f_rmse)
+                        # If ADAM converged to 10x convergence threshold, switch to
+                        # scipy optimization
+                        if (e_rmse/self.N_atoms < self.adam_thresh*0.001 and
+                                f_rmse < self.adam_thresh*0.05):
+                            print('Finished ADAM optimization after %d iterations.'%epoch,
+                                'Total loss = %f, RMSE energy = %f, RMSE forces = %f.'%(
+                                loss_value, e_rmse, f_rmse))
+                            break
 
             # Optimize weights using scipy.minimize
             self.optimizer.minimize(self.session, self.train_dict)
@@ -164,14 +263,51 @@ class NNCalculator(MLCalculator):
             loss_value, e_rmse, f_rmse = self.session.run(
                 [self.loss, self.pot.rmse, self.pot.rmse_forces],
                 self.train_dict)
-            print('Finished optimization %d/%d. '%(i+1,self.opt_restarts) +
-                'Total loss = %f, RMSE energy = %f, RMSE forces = %f.'%(
-                    loss_value, e_rmse, f_rmse))
+            print('Finished optimization %d/%d. '%(i+1, self.opt_restarts) +
+                 'Total loss = %f, RMSE energy = %f, RMSE forces = %f.'%(
+                 loss_value, e_rmse, f_rmse))
             if loss_value < min_loss_value:
                 # save loss value and parameters to restore minimum later
                 min_loss_value = loss_value
                 self.pot.saver.save(self.session,
                     self.model_dir+'min_model.ckpt')
+
+            if e_rmse/self.N_atoms < 0.001 and f_rmse < 0.05:
+                converged = True
+                break
+
+
+        # for i in range(self.opt_restarts):
+        #     # Reset weights to random initialization:
+        #     if (i > 0 or self.reset_fit or
+        #           (self.opt_restarts == 1 and self.reset_fit)):
+        #         self.session.run(tf.initializers.variables(self.pot.variables))
+        #
+        #         # Do a few steps of ADAM optimization to start off:
+        #         self.session.run(self.init_adam)
+        #         for _ in range(self.opt_options['maxiter']):
+        #             self.session.run(self.train, self.train_dict)
+        #         loss_value, e_rmse, f_rmse = self.session.run(
+        #             [self.loss, self.pot.rmse, self.pot.rmse_forces],
+        #             self.train_dict)
+        #         print('Finished Adam optimization. '
+        #             'Total loss = %f, RMSE energy = %f, RMSE forces = %f.'%(
+        #                 loss_value, e_rmse, f_rmse))
+        #
+        #     # Optimize weights using scipy.minimize
+        #     self.optimizer.minimize(self.session, self.train_dict)
+        #
+        #     loss_value, e_rmse, f_rmse = self.session.run(
+        #         [self.loss, self.pot.rmse, self.pot.rmse_forces],
+        #         self.train_dict)
+        #     print('Finished optimization %d/%d. '%(i+1,self.opt_restarts) +
+        #         'Total loss = %f, RMSE energy = %f, RMSE forces = %f.'%(
+        #             loss_value, e_rmse, f_rmse))
+        #     if loss_value < min_loss_value:
+        #         # save loss value and parameters to restore minimum later
+        #         min_loss_value = loss_value
+        #         self.pot.saver.save(self.session,
+        #             self.model_dir+'min_model.ckpt')
 
         self.pot.saver.restore(self.session, self.model_dir+'min_model.ckpt')
         e_rmse, f_rmse = self.session.run(
