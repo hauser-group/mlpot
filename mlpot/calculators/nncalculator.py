91e324a13c01fd4583b3039ed75230fb269a541a
Ralf Meyer <ralf.meyer@tugraz.at>
2019-06-17 17:32:31 +0200

    STY: flake8 formatting

--- mlpot/calculators/nncalculator.py
+++ mlpot/calculators/nncalculator.py
@@ -4,6 +4,7 @@ from mlpot.nnpotentials.utils import calculate_bp_indices
 import numpy as np
 import tensorflow as tf
 
+
 class NNCalculator(MLCalculator):
 
     def __init__(self, restart=None, ignore_bad_restart_file=False,
@@ -14,20 +15,19 @@ class NNCalculator(MLCalculator):
                  maxiter=10000, maxcor=200, miniter=None,
                  e_tol=1e-3, f_tol=5e-2, **kwargs):
         MLCalculator.__init__(self, restart, ignore_bad_restart_file, label,
-                            atoms, C1, C2, **kwargs)
+                              atoms, C1, C2, **kwargs)
 
-        if descriptor_set == None:
-            raise NotImplementedError('For now a descriptor set has to be' +
-                ' passed to the constructor as no default set is implemented')
+        if descriptor_set is None:
+            raise NotImplementedError('For now a descriptor set has to be ' +
+                                      'passed to the constructor as no ' +
+                                      'default set is implemented')
         else:
             self.descriptor_set = descriptor_set
 
         self.atomtypes = self.descriptor_set.atomtypes
 
-        if layers == None:
-            layers = [[5,5] for _ in self.atomtypes]
-        if offsets == None:
-            offsets = [0.0 for _ in self.atomtypes]
+        layers = layers or [[5, 5] for _ in self.atomtypes]
+        offsets = offsets or [0.0 for _ in self.atomtypes]
 
         self.opt_restarts = opt_restarts
         self.reset_fit = reset_fit
@@ -37,9 +37,9 @@ class NNCalculator(MLCalculator):
         self.f_tol = f_tol
 
         self.model_dir = model_dir
-        if not normalize_input in ['mean', 'min_max', 'norm', False]:
+        if normalize_input not in ['mean', 'min_max', 'norm', False]:
             raise NotImplementedError(
-                'Unknown input normalization %s'%normalize_input)
+                'Unknown input normalization %s' % normalize_input)
         self.normalize_input = normalize_input
         # Depending on the type of input normalization these dicts hold
         # different values, for example the mean and the std for "mean" norm
@@ -54,29 +54,33 @@ class NNCalculator(MLCalculator):
 
         self.graph = tf.Graph()
         with self.graph.as_default():
-            self.pot = BPpotential(self.atomtypes,
+            self.pot = BPpotential(
+                self.atomtypes,
                 [self.descriptor_set.num_Gs[self.descriptor_set.type_dict[t]]
-                    for t in self.atomtypes], layers = layers,
-                build_forces = True, offsets = offsets, precision = tf.float64)
+                    for t in self.atomtypes], layers=layers,
+                build_forces=True, offsets=offsets, precision=tf.float64)
 
             with self.graph.name_scope('train'):
                 regularizer = tf.contrib.layers.l2_regularizer(scale=lamb)
-                reg_variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-                #reg_variables = self.pot.variables
+                reg_variables = tf.get_collection(
+                    tf.GraphKeys.REGULARIZATION_LOSSES)
+                # reg_variables = self.pot.variables
                 reg_term = tf.contrib.layers.apply_regularization(
                     regularizer, reg_variables)
                 self.sum_squared_error = tf.reduce_sum(
                     (self.pot.target-self.pot.E_predict)**2)
                 self.sum_squared_error_force = tf.reduce_sum(
                     (self.pot.target_forces-self.pot.F_predict)**2)
-                self.loss = tf.add(.5*self.pot.mse,
+                self.loss = tf.add(
+                    .5*self.pot.mse,
                     .5*self.pot.mse_forces*(self.C2/self.C1)
                     + reg_term/self.C1, name='Loss')
                 self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(
                     self.loss, method=opt_method,
-                    options={'maxiter':maxiter, 'disp':False, 'ftol':1e-14,
-                        'gtol':1e-14, 'maxcor':maxcor},
-                    var_list = self.pot.variables)#[v for v in self.pot.variables if not 'b:' in v.name])
+                    options={'maxiter': maxiter, 'disp': False, 'ftol': 1e-14,
+                             'gtol': 1e-14, 'maxcor': maxcor},
+                    var_list=self.pot.variables)
+                # [v for v in self.pot.variables if not 'b:' in v.name])
         self.session = tf.Session(config=config, graph=self.graph)
         self.session.run(tf.initializers.variables(self.pot.variables))
 
@@ -97,20 +101,21 @@ class NNCalculator(MLCalculator):
         self.F_train.append(atoms.get_forces())
 
         self.int_types.append([self.descriptor_set.type_dict[ti] for ti in
-            atoms.get_chemical_symbols()])
+                               atoms.get_chemical_symbols()])
         Gi, dGi = self.descriptor_set.eval_ase(atoms, derivatives=True)
         self.Gs.append(Gi)
         self.dGs.append(dGi)
 
     def fit(self):
-        print('Fit called with %d geometries.'%len(self.atoms_train))
+        print('Fit called with %d geometries.' % len(self.atoms_train))
         # TODO: This could be streamlined even more by not using the
         # calculate_bp_indices function on the whole set but simply appending
         # to ann_inputs, indices and ann_derivs
 
         ann_inputs, indices, ann_derivs = calculate_bp_indices(
             len(self.atomtypes), self.Gs, self.int_types, dGs=self.dGs)
-        self.train_dict = {self.pot.target: self.E_train,
+        self.train_dict = {
+            self.pot.target: self.E_train,
             self.pot.target_forces: self.F_train,
             self.pot.error_weights: np.ones(len(self.atoms_train))}
         for i, t in enumerate(self.atomtypes):
@@ -118,7 +123,7 @@ class NNCalculator(MLCalculator):
             if self.normalize_input == 'norm':
                 norm_i = np.linalg.norm(ann_inputs[i], axis=1)
                 self.train_dict[self.pot.atomic_contributions[t].input] = (
-                    ann_inputs[i])/norm_i[:,np.newaxis]
+                    ann_inputs[i])/norm_i[:, np.newaxis]
                 self.train_dict[
                     self.pot.atomic_contributions[t].derivatives_input
                     ] = np.einsum('ijkl,i->ijkl', ann_derivs[i], 1.0/norm_i)
@@ -127,7 +132,8 @@ class NNCalculator(MLCalculator):
                     ann_inputs[i]-self.Gs_norm1[t])/self.Gs_norm2[t]
                 self.train_dict[
                     self.pot.atomic_contributions[t].derivatives_input
-                    ] = np.einsum('ijkl,j->ijkl', ann_derivs[i], 1.0/self.Gs_norm2[t])
+                    ] = np.einsum('ijkl,j->ijkl', ann_derivs[i],
+                                  1.0/self.Gs_norm2[t])
 
         if self.normalize_input == 'mean':
             for i, t in enumerate(self.atomtypes):
@@ -136,10 +142,10 @@ class NNCalculator(MLCalculator):
                 self.Gs_norm2[t] = np.std(ann_inputs[i], axis=0) + 1E-6
         elif self.normalize_input == 'min_max':
             for i, t in enumerate(self.atomtypes):
-                self.Gs_norm1[t] =  np.min(ann_inputs[i], axis=0)
+                self.Gs_norm1[t] = np.min(ann_inputs[i], axis=0)
                 # Small offset for numerical stability
                 self.Gs_norm2[t] = (np.max(ann_inputs[i], axis=0) -
-                    np.min(ann_inputs[i], axis=0) + 1E-6)
+                                    np.min(ann_inputs[i], axis=0) + 1E-6)
 
         # Start with large minimum loss value
         min_loss_value = 1E20
@@ -160,6 +166,7 @@ class NNCalculator(MLCalculator):
                 pass
 
             self.opt_step = 0
+
             # Callback function that takes a vector of weights and checks if
             # the optimization is converged. Raises ConvergedNotAnError to stop
             # the scipy optimization if convergence criteria are met.
@@ -174,11 +181,13 @@ class NNCalculator(MLCalculator):
                         # the end of the optimization. Since we are interupting
                         # the optimization (by raising an Exception) the
                         # variables have to be updated at this point.
-                        var_vals = [packed_vars[packing_slice] for packing_slice
+                        var_vals = [
+                            packed_vars[packing_slice] for packing_slice
                             in self.optimizer._packing_slices]
                         self.session.run(
-                            self.optimizer._var_updates, feed_dict=dict(zip(
-                            self.optimizer._update_placeholders, var_vals))
+                            self.optimizer._var_updates,
+                            feed_dict=dict(zip(
+                                self.optimizer._update_placeholders, var_vals))
                         )
                         raise ConvergedNotAnError()
                 self.opt_step = self.opt_step + 1
@@ -186,24 +195,24 @@ class NNCalculator(MLCalculator):
             # Optimize weights using scipy.minimize
             try:
                 self.optimizer.minimize(self.session, self.train_dict,
-                    step_callback=step_callback)
+                                        step_callback=step_callback)
             except ConvergedNotAnError:
-                # This is actually the sucessful convergence of the optimization
-                print('Converged after %d steps.'%self.opt_step)
+                # This is actually the sucessful convergence
+                print('Converged after %d steps.' % self.opt_step)
 
             loss_value, e_rmse, f_rmse = self.session.run(
                 [self.loss, self.pot.rmse, self.pot.rmse_forces],
                 self.train_dict)
-            print('Finished optimization %d/%d after %d steps. '%(
+            print('Finished optimization %d/%d after %d steps. ' % (
                 i+1, self.opt_restarts, self.opt_step) +
-                'Total loss = %f, RMSE energy = %f, RMSE forces = %f.'%(
+                'Total loss = %f, RMSE energy = %f, RMSE forces = %f.' % (
                 loss_value, e_rmse, f_rmse))
             # Save model parameters should this be a new minimum
             if loss_value < min_loss_value:
                 # save loss value and parameters to restore minimum later
                 min_loss_value = loss_value
                 self.pot.saver.save(self.session,
-                    self.model_dir+'min_model.ckpt')
+                                    self.model_dir+'min_model.ckpt')
 
             if e_rmse/self.N_atoms < self.e_tol and f_rmse < self.f_tol:
                 break
@@ -212,23 +221,23 @@ class NNCalculator(MLCalculator):
         e_rmse, f_rmse = self.session.run(
             [self.pot.rmse, self.pot.rmse_forces], self.train_dict)
         print('Fit finished. Final RMSE energy = '
-            '%f, RMSE force = %f.'%(e_rmse, f_rmse))
+              '%f, RMSE force = %f.' % (e_rmse, f_rmse))
 
     def predict(self, atoms):
         int_types = [self.descriptor_set.type_dict[ti] for ti in
-            atoms.get_chemical_symbols()]
+                     atoms.get_chemical_symbols()]
         Gs, dGs = self.descriptor_set.eval_ase(atoms, derivatives=True)
         ann_inputs, indices, ann_derivs = calculate_bp_indices(
-            len(self.atomtypes), [Gs], [int_types], dGs = [dGs])
+            len(self.atomtypes), [Gs], [int_types], dGs=[dGs])
 
         eval_dict = {self.pot.target: np.zeros(1),
-            self.pot.target_forces: np.zeros((1, len(atoms), 3))}
+                     self.pot.target_forces: np.zeros((1, len(atoms), 3))}
         for i, t in enumerate(self.atomtypes):
             eval_dict[self.pot.atom_indices[t]] = indices[i]
             if self.normalize_input == 'norm':
                 norm_i = np.linalg.norm(ann_inputs[i], axis=1)
                 eval_dict[self.pot.atomic_contributions[t].input] = (
-                    ann_inputs[i])/norm_i[:,np.newaxis]
+                    ann_inputs[i])/norm_i[:, np.newaxis]
                 eval_dict[
                     self.pot.atomic_contributions[t].derivatives_input
                     ] = np.einsum('ijkl,i->ijkl', ann_derivs[i], 1.0/norm_i)
@@ -237,17 +246,18 @@ class NNCalculator(MLCalculator):
                     ann_inputs[i]-self.Gs_norm1[t])/self.Gs_norm2[t]
                 eval_dict[
                     self.pot.atomic_contributions[t].derivatives_input
-                    ] = np.einsum('ijkl,j->ijkl', ann_derivs[i], 1.0/self.Gs_norm2[t])
+                    ] = np.einsum('ijkl,j->ijkl', ann_derivs[i],
+                                  1.0/self.Gs_norm2[t])
 
         E = self.session.run(self.pot.E_predict, eval_dict)[0]
         F = self.session.run(self.pot.F_predict, eval_dict)[0]
         return E, F
 
     def get_params(self):
-        params = {'normalize_input':self.normalize_input,
-            'Gs_norm1':self.Gs_norm1, 'Gs_norm2':self.Gs_norm2,
-            'model_dir':self.pot.saver.save(
-                self.session, self.model_dir+'model.ckpt')}
+        params = {'normalize_input': self.normalize_input,
+                  'Gs_norm1': self.Gs_norm1, 'Gs_norm2': self.Gs_norm2,
+                  'model_dir': self.pot.saver.save(
+                    self.session, self.model_dir+'model.ckpt')}
         return params
 
     def set_params(self, **params):
