4b4534a50231f482a0bfc2cda6201ba39c445ccb
Ralf Meyer <ralf.meyer@tugraz.at>
2019-07-17 17:30:57 +0200

    TST: change structure of TestCase super class

--- test/test_kernels/test_kernels.py
+++ test/test_kernels/test_kernels.py
@@ -9,12 +9,10 @@ from mlpot.kernels import (ConstantKernel, RBFKernel, RBFKernel_with_factor,
 class KernelTest():
     class KernelTest(unittest.TestCase):
         def test_atomic_position_gradient(self):
-            kernel = self.kernel(**self.kwargs)
-
             atomsX = np.random.randn(1, 12)
             atomsY = np.random.randn(1, 12)
 
-            K = kernel(atomsX, atomsY, dx=True, dy=True)
+            K = self.kernel(atomsX, atomsY, dx=True, dy=True)
             dK_num_total = np.zeros_like(K)
             dK_num_total[0, 0] = K[0, 0]
 
@@ -22,8 +20,8 @@ class KernelTest():
             for i in range(12):
                 dxi = np.zeros(12)
                 dxi[i] = dx
-                K_plus = kernel(atomsX+dxi, atomsY, dx=True, dy=True)
-                K_minus = kernel(atomsX-dxi, atomsY, dx=True, dy=True)
+                K_plus = self.kernel(atomsX+dxi, atomsY, dx=True, dy=True)
+                K_minus = self.kernel(atomsX-dxi, atomsY, dx=True, dy=True)
                 # Test first derivative
                 dK_num_total[1+i, 0] = (K_plus[0, 0] - K_minus[0, 0])/(2*dx)
                 # Approximate second derivative as numerical derivative of
@@ -31,34 +29,33 @@ class KernelTest():
                 dK_num_total[1+i, 1:] = (K_plus[0, 1:] - K_minus[0, 1:])/(2*dx)
 
                 # Test symmetry of derivatives
-                K_plus = kernel(atomsX, atomsY+dxi, dx=True, dy=True)
-                K_minus = kernel(atomsX, atomsY-dxi, dx=True, dy=True)
+                K_plus = self.kernel(atomsX, atomsY+dxi, dx=True, dy=True)
+                K_minus = self.kernel(atomsX, atomsY-dxi, dx=True, dy=True)
                 dK_num_total[0, 1+i] = (K_plus[0, 0] - K_minus[0, 0])/(2*dx)
 
             np.testing.assert_allclose(K, dK_num_total, atol=1E-9)
 
         def test_hyperparameter_gradient(self):
-            kernel = self.kernel(**self.kwargs)
             atomsX = np.random.randn(5, 12)
             atomsY = np.random.randn(5, 12)
 
-            K, dK = kernel(atomsX, atomsY, dx=True, dy=True,
-                           eval_gradient=True)
+            K, dK = self.kernel(atomsX, atomsY, dx=True, dy=True,
+                                eval_gradient=True)
             dK_num = np.zeros_like(dK)
 
             # Derivative with respect to the hyperparameters:
             dt = 1e-6
             # Hyperparameters live on an exponential scale!!!
-            for i in range(len(kernel.theta)):
-                dti = np.zeros(len(kernel.theta))
+            for i in range(len(self.kernel.theta)):
+                dti = np.zeros(len(self.kernel.theta))
                 dti[i] = dt
-                kernel.theta = np.log(np.exp(kernel.theta) + dti)
-                K_plus = kernel(atomsX, atomsY, dx=True, dy=True,
-                                eval_gradient=False)
-                kernel.theta = np.log(np.exp(kernel.theta) - 2*dti)
-                K_minus = kernel(atomsX, atomsY, dx=True, dy=True,
-                                 eval_gradient=False)
-                kernel.theta = np.log(np.exp(kernel.theta) + dti)
+                self.kernel.theta = np.log(np.exp(self.kernel.theta) + dti)
+                K_plus = self.kernel(atomsX, atomsY, dx=True, dy=True,
+                                     eval_gradient=False)
+                self.kernel.theta = np.log(np.exp(self.kernel.theta) - 2*dti)
+                K_minus = self.kernel(atomsX, atomsY, dx=True, dy=True,
+                                      eval_gradient=False)
+                self.kernel.theta = np.log(np.exp(self.kernel.theta) + dti)
                 dK_num[:, :, i] = (K_plus - K_minus)/(2*dt)
 
             np.testing.assert_allclose(dK, dK_num, atol=1E-8)
@@ -68,56 +65,51 @@ class KernelTest():
             should only ever be needed when the kernel is called with the same
             arguments. At least that is how scikit-learn implements kernels.
             """
-            kernel = self.kernel(**self.kwargs)
             atomsX = np.random.randn(5, 12)
 
-            K, dK = kernel(atomsX, atomsX, dx=True, dy=True,
-                           eval_gradient=True)
+            K, dK = self.kernel(atomsX, atomsX, dx=True, dy=True,
+                                eval_gradient=True)
             dK_num = np.zeros_like(dK)
 
             # Derivative with respect to the hyperparameters:
             dt = 1e-6
             # Hyperparameters live on an exponential scale!!!
-            for i in range(len(kernel.theta)):
-                dti = np.zeros(len(kernel.theta))
+            for i in range(len(self.kernel.theta)):
+                dti = np.zeros(len(self.kernel.theta))
                 dti[i] = dt
-                kernel.theta = np.log(np.exp(kernel.theta) + dti)
-                K_plus = kernel(atomsX, atomsX, dx=True, dy=True,
-                                eval_gradient=False)
-                kernel.theta = np.log(np.exp(kernel.theta) - 2*dti)
-                K_minus = kernel(atomsX, atomsX, dx=True, dy=True,
-                                 eval_gradient=False)
-                kernel.theta = np.log(np.exp(kernel.theta) + dti)
+                self.kernel.theta = np.log(np.exp(self.kernel.theta) + dti)
+                K_plus = self.kernel(atomsX, atomsX, dx=True, dy=True,
+                                     eval_gradient=False)
+                self.kernel.theta = np.log(np.exp(self.kernel.theta) - 2*dti)
+                K_minus = self.kernel(atomsX, atomsX, dx=True, dy=True,
+                                      eval_gradient=False)
+                self.kernel.theta = np.log(np.exp(self.kernel.theta) + dti)
                 dK_num[:, :, i] = (K_plus - K_minus)/(2*dt)
 
             np.testing.assert_allclose(dK, dK_num, atol=1E-8)
 
         def test_symmetry(self):
-            kernel = self.kernel(**self.kwargs)
             atomsX = np.random.randn(1, 12)
 
-            K = kernel(atomsX, atomsX, dx=True, dy=True)
+            K = self.kernel(atomsX, atomsX, dx=True, dy=True)
 
             np.testing.assert_allclose(K, K.T)
 
         def test_diag(self):
-            kernel = self.kernel(**self.kwargs)
             atomsX = np.random.randn(5, 12)
 
-            K = kernel(atomsX, atomsX, dx=True, dy=True)
-            diag_K = kernel.diag(atomsX)
+            K = self.kernel(atomsX, atomsX, dx=True, dy=True)
+            diag_K = self.kernel.diag(atomsX)
 
             np.testing.assert_allclose(diag_K, np.diag(K), rtol=1E-6)
 
 
 class ConstantKernelTest(KernelTest.KernelTest):
-    kernel = ConstantKernel
-    kwargs = {'constant': 23.4}
+    kernel = ConstantKernel(constant=23.4)
 
 
 class RBFKernelTest(KernelTest.KernelTest):
-    kernel = RBFKernel
-    kwargs = {'constant': 23.4, 'factor': 1.234, 'length_scale': 1.321}
+    kernel = RBFKernel(constant=23.4, factor=1.234, length_scale=1.321)
 
     def test_symmetry(self):
         atomsX = np.random.randn(1, 12)
@@ -158,26 +150,22 @@ class RBFKernelTest(KernelTest.KernelTest):
 
 
 class RBFKernelAnisoTest(KernelTest.KernelTest):
-    kernel = RBFKernel
-    kwargs = {'constant': 23.4, 'factor': 1.234,
-              'length_scale': np.array([1.321]*12)}
+    kernel = RBFKernel(constant=23.4, factor=1.234,
+                       length_scale=np.array([1.321]*12))
 
 
 class RBFKernel_with_factorTest(KernelTest.KernelTest):
-    kernel = RBFKernel_with_factor
-    kwargs = {'constant': 23.4, 'factor': 1.234,
-              'length_scale': 1.321}
+    kernel = RBFKernel_with_factor(constant=23.4, factor=1.234,
+                                   length_scale=1.321)
 
 
 class RBFKernel_with_factorAnisoTest(KernelTest.KernelTest):
-    kernel = RBFKernel_with_factor
-    kwargs = {'constant': 23.4, 'factor': 1.234,
-              'length_scale': np.array([1.321]*12)}
+    kernel = RBFKernel_with_factor(constant=23.4, factor=1.234,
+                                   length_scale=np.array([1.321]*12))
 
 
 class MaternKernelTest(KernelTest.KernelTest):
-    kernel = MaternKernel
-    kwargs = {'constant': 23.4, 'factor': 1.234, 'length_scale': 1.321}
+    kernel = MaternKernel(constant=23.4, factor=1.234, length_scale=1.321)
 
     def test_symmetry(self):
         atomsX = np.random.randn(1, 12)
@@ -202,14 +190,12 @@ class MaternKernelTest(KernelTest.KernelTest):
 
 
 class MaternKernelAnisoTest(KernelTest.KernelTest):
-    kernel = MaternKernel
-    kwargs = {'constant': 23.4, 'factor': 1.234,
-              'length_scale': np.array([1.321]*12)}
+    kernel = MaternKernel(constant=23.4, factor=1.234,
+                          length_scale=np.array([1.321]*12))
 
 
 class DotProductKernelTest(KernelTest.KernelTest):
-    kernel = DotProductKernel
-    kwargs = {'sigma0': 23.4}
+    kernel = DotProductKernel(sigma0=2.34)
 
     def test_symmetry(self):
         atomsX = np.random.randn(1, 12)
@@ -226,39 +212,38 @@ class DotProductKernelTest(KernelTest.KernelTest):
 
 
 class DotProductKernelExp1Test(KernelTest.KernelTest):
-    kernel = DotProductKernel
-    kwargs = {'sigma0': 23.4, 'exponent': 1}
+    kernel = DotProductKernel(sigma0=2.34, exponent=1)
 
 
 class DotProductKernelExp4Test(KernelTest.KernelTest):
-    kernel = DotProductKernel
-    kwargs = {'sigma0': 23.4, 'exponent': 4}
+    kernel = DotProductKernel(sigma0=23.4, exponent=4)
 
 
 class NormalizedDotProductKernelTest(KernelTest.KernelTest):
-    kernel = NormalizedDotProductKernel
-    kwargs = {'sigma0': 4.321, 'constant': 1.234}
+    kernel = NormalizedDotProductKernel(sigma0=4.321, constant=1.234)
 
 
 class NormalizedDotProductKernelExp1Test(KernelTest.KernelTest):
-    kernel = NormalizedDotProductKernel
-    kwargs = {'sigma0': 4.321, 'constant': 1.234, 'exponent': 1}
+    kernel = NormalizedDotProductKernel(sigma0=4.321, constant=1.234,
+                                        exponent=1)
 
 
 class NormalizedDotProductKernelExp4Test(KernelTest.KernelTest):
-    kernel = NormalizedDotProductKernel
-    kwargs = {'sigma0': 4.321, 'constant': 1.234, 'exponent': 4}
+    kernel = NormalizedDotProductKernel(sigma0=4.321, constant=1.234,
+                                        exponent=4)
 
 
 class NormalizedDotProductKernelwithHyperparameterTest(KernelTest.KernelTest):
-    kernel = NormalizedDotProductKernelwithHyperparameter
-    kwargs = {'sigma0': 4.321, 'exponent': 2, 'constant': 1.234}
+    kernel = NormalizedDotProductKernelwithHyperparameter(
+        sigma0=4.321, exponent=2, constant=1.234
+    )
 
 
 class NormalizedDotProductKernelwithHyperparameterExp4Test(
         KernelTest.KernelTest):
-    kernel = NormalizedDotProductKernelwithHyperparameter
-    kwargs = {'sigma0': 4.321, 'exponent': 4, 'constant': 1.234}
+    kernel = NormalizedDotProductKernelwithHyperparameter(
+        sigma0=4.321, exponent=4, constant=1.234
+    )
 
 
 if __name__ == '__main__':
