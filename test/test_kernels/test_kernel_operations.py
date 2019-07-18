ba7da6d5ac1731ce79d976f64a38a680b1511cad
Ralf Meyer <ralf.meyer@tugraz.at>
2019-07-18 19:59:49 +0200

    TST: TestCase for Exponentation operation

--- test/test_kernels/test_kernel_operations.py
+++ test/test_kernels/test_kernel_operations.py
@@ -1,6 +1,6 @@
 import numpy as np
 import unittest
-from mlpot.kernels import (Sum, Product, Rescaling,
+from mlpot.kernels import (Sum, Product, Rescaling, Exponentiation,
                            ConstantKernel, RBFKernel,
                            DotProductKernel)
 try:
@@ -105,7 +105,8 @@ class RBFtimesRBFTest(KernelTest.KernelTest):
 
 
 class DottimeDotTest(KernelTest.KernelTest):
-    kernel = Product(DotProductKernel(), DotProductKernel())
+    kernel = Product(DotProductKernel(sigma0=0.1),
+                     DotProductKernel(sigma0=0.1))
 
     def test_2times2is4(self):
         n = 3
@@ -145,11 +146,8 @@ class RescalingTest(KernelTest.KernelTest):
         np.testing.assert_allclose(dK1, dK2)
 
     def test__rmul__(self):
-        n = 3
-        m = 4
-        n_dim = 12
-        X = np.random.randn(n, n_dim)
-        Y = np.random.randn(m, n_dim)
+        X = np.random.randn(3, 4)
+        Y = np.random.randn(3, 4)
 
         factor = 10.0
 
@@ -164,11 +162,8 @@ class RescalingTest(KernelTest.KernelTest):
         np.testing.assert_allclose(dK1, dK2)
 
     def test__mul__(self):
-        n = 3
-        m = 4
-        n_dim = 12
-        X = np.random.randn(n, n_dim)
-        Y = np.random.randn(m, n_dim)
+        X = np.random.randn(3, 4)
+        Y = np.random.randn(3, 4)
 
         factor = 10.0
 
@@ -181,6 +176,48 @@ class RescalingTest(KernelTest.KernelTest):
 
         np.testing.assert_allclose(K1, K2)
         np.testing.assert_allclose(dK1, dK2)
+
+
+class ExponentiationTest(KernelTest.KernelTest):
+    kernel = Exponentiation(RBFKernel(), 2.0)
+
+    def test_rbf_squared(self):
+        X = np.random.randn(3, 4)
+        Y = np.random.randn(3, 4)
+
+        kernel1 = Exponentiation(RBFKernel(length_scale=2.0), exponent=4)
+        kernel2 = RBFKernel(length_scale=1.0)
+
+        K1, dK1 = kernel1(X, Y, dx=True, dy=True, eval_gradient=True)
+        K2, dK2 = kernel2(X, Y, dx=True, dy=True, eval_gradient=True)
+
+        np.testing.assert_allclose(K1, K2)
+        np.testing.assert_allclose(2*dK1, dK2)
+
+    def test_dot_product(self):
+        X = np.random.randn(3, 4)
+        Y = np.random.randn(3, 4)
+
+        kernel1 = Exponentiation(DotProductKernel(exponent=2), exponent=2)
+        kernel2 = DotProductKernel(exponent=4)
+
+        K1 = kernel1(X, Y, dx=True, dy=True)
+        K2 = kernel2(X, Y, dx=True, dy=True)
+
+        np.testing.assert_allclose(K1, K2)
+
+    def test_comparison2product(self):
+        X = np.random.randn(3, 4)
+        Y = np.random.randn(3, 4)
+
+        kernel1 = Exponentiation(RBFKernel(length_scale=1.23), exponent=2)
+        kernel2 = Product(RBFKernel(length_scale=1.23),
+                          RBFKernel(length_scale=1.23))
+
+        K1 = kernel1(X, Y, dx=True, dy=True)
+        K2 = kernel2(X, Y, dx=True, dy=True)
+
+        np.testing.assert_allclose(K1, K2)
 
 
 if __name__ == '__main__':
