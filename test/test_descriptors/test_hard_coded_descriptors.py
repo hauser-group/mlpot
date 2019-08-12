3a5065975f076e6db8b611c33add1c6e33f73af0
Ralf Meyer <ralf.meyer@tugraz.at>
2019-08-12 09:44:03 +0200

    TST: test to compare the hard coded and auto generated versions of G4

--- test/test_descriptors/test_hard_coded_descriptors.py
+++ test/test_descriptors/test_hard_coded_descriptors.py
@@ -24,15 +24,12 @@ class HardCodedTest(unittest.TestCase):
 
                 N = 30
                 r_vec = np.linspace(1., 5., N)
-                theta_vec = np.linspace(0.3*np.pi, 2.*np.pi, N, endpoint=True)
+                theta_vec = np.linspace(0.1*np.pi, 1.9*np.pi, N, endpoint=True)
                 for ri in r_vec:
                     for ti in theta_vec:
                         xyzs = np.array([[0.0, 0.0, 0.0],
                                         [0.5*ri, 0.0, 0.0],
                                         [ri*np.cos(ti), ri*np.sin(ti), 0.0]])
-                        print(ti, 0.01*np.pi, np.cos(ti), np.sin(ti),
-                              np.cos(ti)**2 + np.sin(ti)**2)
-                        print(xyzs)
 
                         G1 = ds1.eval_atomwise(types, xyzs)
                         G2 = ds2.eval_atomwise(types, xyzs)
@@ -40,8 +37,6 @@ class HardCodedTest(unittest.TestCase):
 
                         dG1 = ds1.eval_derivatives_atomwise(types, xyzs)
                         dG2 = ds2.eval_derivatives_atomwise(types, xyzs)
-                        print(dG1[0][0])
-                        print(dG2[0][0])
                         np.testing.assert_allclose(dG1, dG2,
                                                    equal_nan=False, atol=1e-15)
 
@@ -49,9 +44,9 @@ class HardCodedTest(unittest.TestCase):
                             ds1.eval_with_derivatives_atomwise(types, xyzs))
                         G2, dG2 = (
                             ds2.eval_with_derivatives_atomwise(types, xyzs))
-                        #np.testing.assert_allclose(G1, G2, equal_nan=False)
-                        #np.testing.assert_allclose(dG1, dG2,
-                        #                           equal_nan=False, atol=1e-15)
+                        np.testing.assert_allclose(G1, G2, equal_nan=False)
+                        np.testing.assert_allclose(dG1, dG2,
+                                                   equal_nan=False, atol=1e-15)
 
 
 if __name__ == '__main__':
