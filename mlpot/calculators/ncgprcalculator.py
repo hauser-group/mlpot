96a41537d2f6d7b97a81c9f44c470ff1e070259f
Ralf Meyer <ralf.meyer@tugraz.at>
2019-08-26 09:30:30 +0200

    ENH: Add non-cartesian gpr calculator

---
+++ mlpot/calculators/ncgprcalculator.py
@@ -0,0 +1,148 @@
+from mlpot.calculators.mlcalculator import MLCalculator
+from mlpot.calculators.gprcalculator import GPRCalculator
+import numpy as np
+
+
+class NCGPRCalculator(GPRCalculator):
+    def __init__(self, restart=None, ignore_bad_restart_file=False,
+                 label=None, atoms=None, C1=1e8, C2=1e8,
+                 kernel=None, input_transform=None, opt_method='L-BFGS-B',
+                 opt_restarts=0, normalize_y=False, mean_model=None, **kwargs):
+        GPRCalculator.__init__(self, restart, ignore_bad_restart_file, label,
+                               atoms, C1, C2, kernel=kernel,
+                               opt_method=opt_method,
+                               opt_restarts=opt_restarts,
+                               normalize_y=normalize_y, mean_model=mean_model,
+                               **kwargs)
+        self.input_transform = input_transform
+
+    def add_data(self, atoms):
+        # If the trainings set is empty: setup the numpy arrays
+        if not self.atoms_train:
+            self.n_dim = 3*len(atoms)
+            self.E_train = np.zeros(0)
+            self.F_train = np.zeros(0)
+            self.q_train = []
+            self.dq_train = []
+        # else: check if the new atoms object has the same length as previous
+        else:
+            if not 3*len(atoms) == self.n_dim:
+                raise ValueError('New data does not have the same number of '
+                                 'atoms as previously added data.')
+
+        # Call the super class routine after checking for empty trainings set!
+        MLCalculator.add_data(self, atoms)
+        # Call forces first in case forces and energy are calculated at the
+        # same time by the calculator
+        if self.mean_model is None:
+            F = atoms.get_forces().flatten()
+            E = atoms.get_potential_energy()
+        else:
+            F = (atoms.get_forces().flatten()
+                 - self.mean_model.get_forces(atoms=atoms).flatten())
+            E = (atoms.get_potential_energy()
+                 - self.mean_model.get_potential_energy(atoms=atoms))
+        self.E_train = np.append(self.E_train, E)
+        self.F_train = np.append(self.F_train, F)
+
+        q, dq = self._transform_input(atoms)
+        self.q_train.append(q)
+        self.dq_train.append(dq)
+
+    def _transform_input(self, atoms):
+        return self.input_transform(atoms)
+
+    def _normalize_input(self, x):
+        return x
+
+    def build_kernel_matrix(self, X_star=None, eval_gradient=False):
+        n = len(self.atoms_train)
+        q_X = np.array(self.q_train)
+        dq_X = np.array(self.dq_train)
+        n_qs = q_X.shape[1]
+
+        if X_star is None:
+            m = n
+            q_Y = q_X
+            dq_Y = dq_X
+        else:
+            m = 1
+            q_Y = np.atleast_2d(X_star[0])
+            # Ugly but a quick fix for np.atleast_3d appending the new 3rd
+            # dimension to the end
+            dq_Y = np.array([X_star[1]])
+
+        kernel_mat = np.zeros((n*(1+self.n_dim), m*(1+self.n_dim)))
+        if eval_gradient:
+            # kernel in q coordinates
+            K_q, dK_q = self.kernel(q_X, q_Y, dx=True, dy=True,
+                                    eval_gradient=True)
+        else:
+            K_q = self.kernel(q_X, q_Y, dx=True, dy=True)
+        # no transformation needed for function values
+        kernel_mat[:n, :m] = K_q[:n, :m]
+        # i and j are used as index for the cartesian coordinates
+        # k and l used as index for the q coordinates
+        # K_q[n:, :m] size n*n_qs x m reshape to n x n_qs x m
+        # dq_X size n x n_qs x n_dim
+        kernel_mat[n:, :m] = np.einsum(
+                'nkm,nki->nim', K_q[n:, :m].reshape(n, n_qs, m), dq_X
+            ).reshape(n*self.n_dim, m)
+        # K_q[:n, m:] size n x m*n_qs reshape to n x m x n_qs
+        # dq_Y size m x n_qs x n_dim
+        kernel_mat[:n, m:] = np.einsum(
+                'nml,mlj->nmj', K_q[:n, m:].reshape(n, m, n_qs), dq_Y
+            ).reshape(n, m*self.n_dim)
+        # K_q[n:, m:] size n*n_qs x m*n_qs reshape to n x n_qs x m x n_qs
+        kernel_mat[n:, m:] = np.einsum(
+                'nki,nkml,mlj->nimj',
+                dq_X, K_q[n:, m:].reshape(n, n_qs, m, n_qs),
+                dq_Y, optimize=True).reshape(n*self.n_dim, m*self.n_dim)
+
+        if eval_gradient:
+            n_t = len(self.kernel.theta)
+            kernel_grad = np.zeros(
+                (n*(1+self.n_dim), m*(1+self.n_dim), n_t))
+            kernel_grad[:n, :m, :] = dK_q[:n, :m, :]
+            # Works just the same way as shown above with the additional
+            # index t for the hyperparameters
+            kernel_grad[n:, :m, :] = np.einsum(
+                    'nkmt,nki->nimt',
+                    dK_q[n:, :m, :].reshape(n, n_qs, m, n_t), dq_X
+                ).reshape(n*self.n_dim, m, n_t)
+            kernel_grad[:n, m:, :] = np.einsum(
+                    'nmlt,mlj->nmjt',
+                    dK_q[:n, m:, :].reshape(n, m, n_qs, n_t), dq_Y
+                ).reshape(n, m*self.n_dim, n_t)
+            kernel_grad[n:, m:, :] = np.einsum(
+                    'nki,nkmlt,mlj->nimjt', dq_X,
+                    dK_q[n:, m:, :].reshape(n, n_qs, m, n_qs, n_t),
+                    dq_Y, optimize=True
+                ).reshape(n*self.n_dim, m*self.n_dim, n_t)
+            return kernel_mat, kernel_grad
+        else:
+            return kernel_mat
+
+    def build_kernel_diagonal(self, X_star):
+        """Evaluates the diagonal of the kernel matrix which is needed for
+        the uncertainty prediction. TODO: reaccess if there is a better way to
+        do this.
+        """
+        q_star = np.atleast_2d(X_star[0])
+        # Ugly but a quick fix for np.atleast_3d appending the new 3rd
+        # dimension to the end
+        if np.ndim(X_star[1]) == 2:
+            dq_star = np.array([X_star[1]])
+        elif np.ndim(X_star[1]) == 3:
+            dq_star = np.array(X_star[1])
+        n = q_star.shape[0]
+        n_qs = q_star.shape[1]
+
+        K_q = self.kernel(q_star, q_star, dx=True, dy=True)
+        kernel_diag = np.zeros((n*(1+self.n_dim)))
+        kernel_diag[:n] = K_q.diagonal()[:n]
+        kernel_diag[n:] = np.einsum(
+                'nki,nknl,nli->ni',
+                dq_star, K_q[n:, n:].reshape(n, n_qs, n, n_qs),
+                dq_star, optimize=True).flatten()
+        return kernel_diag
---
+++ test/test_calculators/test_ncgpr_calculator.py
@@ -0,0 +1,160 @@
+import unittest
+import numpy as np
+
+from ase.atoms import Atoms
+from ase.calculators.emt import EMT
+from mlpot.calculators.ncgprcalculator import NCGPRCalculator
+from mlpot.kernels import RBFKernel
+
+
+class GAPCalculatorTest(unittest.TestCase):
+    def test_co_potential_curve(self):
+        direction = np.array([1., 2., 3.])
+        direction /= np.linalg.norm(direction)
+        r_train = np.linspace(0.5, 7, 11)
+        energies_train = []
+        images_train = []
+        for ri in r_train:
+            image = Atoms(
+                ['C', 'O'],
+                positions=np.array([-0.5*ri*direction, 0.5*ri*direction]))
+            image.set_calculator(EMT())
+            energies_train.append(image.get_potential_energy())
+            images_train.append(image)
+
+        def to_radius(x):
+            xyzs = x.get_positions()
+            r = np.sqrt(np.sum((xyzs[1, :]-xyzs[0, :])**2))
+            dr = np.zeros((1,6))
+            dr[0, 0] = (xyzs[0, 0] - xyzs[1, 0])/r
+            dr[0, 1] = (xyzs[0, 1] - xyzs[1, 1])/r
+            dr[0, 2] = (xyzs[0, 2] - xyzs[1, 2])/r
+            dr[0, 3] = (xyzs[1, 0] - xyzs[0, 0])/r
+            dr[0, 4] = (xyzs[1, 1] - xyzs[0, 1])/r
+            dr[0, 5] = (xyzs[1, 2] - xyzs[0, 2])/r
+            return [r], dr
+
+        kernel = RBFKernel(constant=100.0, length_scale=1e-2)
+        calc = NCGPRCalculator(input_transform=to_radius, kernel=kernel,
+                             C1=1e8, C2=1e8, opt_restarts=0)
+
+        [calc.add_data(im) for im in images_train]
+        calc.fit()
+        np.testing.assert_allclose(
+            energies_train,
+            [calc.predict(im)[0] for im in images_train])
+
+    def test_build_diag(self):
+        direction = np.array([1., 2., 3.])
+        direction /= np.linalg.norm(direction)
+        r_train = np.linspace(0.5, 7, 11)
+        energies_train = []
+        images_train = []
+        for ri in r_train:
+            image = Atoms(
+                ['C', 'O'],
+                positions=np.array([-0.5*ri*direction, 0.5*ri*direction]))
+            image.set_calculator(EMT())
+            energies_train.append(image.get_potential_energy())
+            images_train.append(image)
+
+        def to_radius(x):
+            xyzs = x.get_positions()
+            r = np.sqrt(np.sum((xyzs[1, :]-xyzs[0, :])**2))
+            dr = np.zeros((1,6))
+            dr[0, 0] = (xyzs[0, 0] - xyzs[1, 0])/r
+            dr[0, 1] = (xyzs[0, 1] - xyzs[1, 1])/r
+            dr[0, 2] = (xyzs[0, 2] - xyzs[1, 2])/r
+            dr[0, 3] = (xyzs[1, 0] - xyzs[0, 0])/r
+            dr[0, 4] = (xyzs[1, 1] - xyzs[0, 1])/r
+            dr[0, 5] = (xyzs[1, 2] - xyzs[0, 2])/r
+            return [r], dr
+
+        kernel = RBFKernel(constant=100.0, length_scale=1e-2)
+        calc = NCGPRCalculator(input_transform=to_radius, kernel=kernel,
+                             C1=1e8, C2=1e8, opt_restarts=0)
+
+        [calc.add_data(im) for im in images_train]
+        calc.fit()
+        np.testing.assert_allclose(
+            calc.build_kernel_diagonal((calc.q_train, calc.dq_train)),
+            np.diag(calc.build_kernel_matrix()))
+
+
+    def test_kernel_matrix_derivative(self):
+        direction = np.array([1., 2., 3.])
+        direction /= np.linalg.norm(direction)
+        atoms = Atoms(
+            ['C', 'O'],
+            positions=np.array([-0.5*direction, 0.5*direction]))
+        atoms.set_calculator(EMT())
+
+        def to_radius(x):
+            xyzs = x.get_positions()
+            r = np.sqrt(np.sum((xyzs[1, :]-xyzs[0, :])**2))
+            dr = np.zeros((1,6))
+            dr[0, 0] = (xyzs[0, 0] - xyzs[1, 0])/r
+            dr[0, 1] = (xyzs[0, 1] - xyzs[1, 1])/r
+            dr[0, 2] = (xyzs[0, 2] - xyzs[1, 2])/r
+            dr[0, 3] = (xyzs[1, 0] - xyzs[0, 0])/r
+            dr[0, 4] = (xyzs[1, 1] - xyzs[0, 1])/r
+            dr[0, 5] = (xyzs[1, 2] - xyzs[0, 2])/r
+            return [r], dr
+
+        kernel = RBFKernel(constant=100.0, length_scale=0.23)
+        calc = NCGPRCalculator(input_transform=to_radius, kernel=kernel,
+                             C1=1e8, C2=1e8, opt_restarts=0)
+        calc.add_data(atoms)
+        K = calc.build_kernel_matrix()
+        K_num = np.zeros_like(K)
+        # kernel value is not tested:
+        K_num[0, 0] = K[0, 0]
+        x0 = atoms.get_positions()
+        dx = 1e-4
+
+        def num_dx_forth_order(fun, x0, y0, dx):
+            return (8*fun(x0+dx, y0) - fun(x0+2*dx, y0)
+                    - 8*fun(x0-dx, y0) + fun(x0-2*dx, y0)
+                    )/(12*np.linalg.norm(dx))
+
+        def num_dy_forth_order(fun, x0, y0, dy):
+            return (8*fun(x0, y0+dy) - fun(x0, y0+2*dy)
+                    - 8*fun(x0, y0-dy) + fun(x0, y0-2*dy)
+                    )/(12*np.linalg.norm(dx))
+
+        def num_dxdy_forth_order(fun, x0, y0, dx, dy):
+            return (64*fun(x0+dx, y0+dy) - 8*fun(x0+dx, y0+2*dy)
+                    - 64*fun(x0+dx, y0-dy) + 8*fun(x0+dx, y0-2*dy)
+                    - 8*fun(x0+2*dx, y0+dy) + fun(x0+2*dx, y0+2*dy)
+                    + 8*fun(x0+2*dx, y0-dy) - fun(x0+2*dx, y0-2*dy)
+                    - 64*fun(x0-dx, y0+dy) + 8*fun(x0-dx, y0+2*dy)
+                    + 64*fun(x0-dx, y0-dy) - 8*fun(x0-dx, y0-2*dy)
+                    + 8*fun(x0-2*dx, y0+dy) - fun(x0-2*dx, y0+2*dy)
+                    - 8*fun(x0-2*dx, y0-dy) + fun(x0-2*dx, y0-2*dy)
+                    )/(144*np.linalg.norm(dx)*np.linalg.norm(dy))
+
+        def K_fun(x, y):
+            a = np.array([to_radius(Atoms(['C', 'O'], positions=x))[0]])
+            b = np.array([to_radius(Atoms(['C', 'O'], positions=y))[0]])
+            return calc.kernel(a, b, dx=True, dy=True)[0, 0]
+
+        for i in range(6):
+            dxi = np.zeros(6)
+            dxi[i] = dx
+            dxi = dxi.reshape((2,3))
+            # Test first derivative
+            K_num[1+i, 0] = num_dx_forth_order(K_fun, x0, x0, dxi)
+
+            for j in range(6):
+                dxj = np.zeros(6)
+                dxj[j] = dx
+                dxj = dxj.reshape((2,3))
+                K_num[1+i, 1+j] = num_dxdy_forth_order(K_fun, x0, x0, dxi, dxj)
+
+            # Test symmetry of derivatives
+            K_num[0, 1+i] = num_dy_forth_order(K_fun, x0, x0, dxi)
+        np.testing.assert_allclose(K, K_num, atol=1E-5)
+
+
+if __name__ == '__main__':
+    unittest.main()
